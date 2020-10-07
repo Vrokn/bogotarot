586475e9d806968db190bd03d3345550c4a57fec
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-10-07 01:33:12 -0500

    Updates

---
+++ precache-manifest.bd88fbedc3cb541517d6eb7bfa50e463.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325bb7239d59aef49053d20b5f7cf90f",
+    "url": "/bogotarot/index.html"
+  },
+  {
+    "revision": "26cb8dc39e4b556dce39",
+    "url": "/bogotarot/static/css/2.84637b4e.chunk.css"
+  },
+  {
+    "revision": "012fbc633e9a659a09e5",
+    "url": "/bogotarot/static/css/main.bf9e4ea7.chunk.css"
+  },
+  {
+    "revision": "26cb8dc39e4b556dce39",
+    "url": "/bogotarot/static/js/2.65b1a63a.chunk.js"
+  },
+  {
+    "revision": "af945f101cccf5f437e55a3044282b78",
+    "url": "/bogotarot/static/js/2.65b1a63a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012fbc633e9a659a09e5",
+    "url": "/bogotarot/static/js/main.6728d7b6.chunk.js"
+  },
+  {
+    "revision": "cb88a131257a3fcbb313",
+    "url": "/bogotarot/static/js/runtime-main.52364768.js"
+  },
+  {
+    "revision": "0e00652849f7703bb37841ba7c3d41b4",
+    "url": "/bogotarot/static/media/CartaAzul.0e006528.svg"
+  },
+  {
+    "revision": "a59be02572c3ead2dad54212c271fe2d",
+    "url": "/bogotarot/static/media/Historias.a59be025.svg"
+  },
+  {
+    "revision": "ddeeebbc57ee4a95b844aedfe052d2ad",
+    "url": "/bogotarot/static/media/Historiasmobil.ddeeebbc.svg"
+  },
+  {
+    "revision": "fa0b5bdf9f7bbd4a16e24cad24d01868",
+    "url": "/bogotarot/static/media/Instrucciones.fa0b5bdf.svg"
+  },
+  {
+    "revision": "fa33a900262828ce995c293cbe50e5dd",
+    "url": "/bogotarot/static/media/Instruccionesmobile.fa33a900.svg"
+  },
+  {
+    "revision": "0142c6e7a1394274136772a66a310c38",
+    "url": "/bogotarot/static/media/Logosidpcmdb.0142c6e7.svg"
+  },
+  {
+    "revision": "e710b2cef323ef0d9130545914afb687",
+    "url": "/bogotarot/static/media/Ojoamarillo.e710b2ce.svg"
+  },
+  {
+    "revision": "17f55e3cd76b2989fd1f38a6c79dd40b",
+    "url": "/bogotarot/static/media/Presentación.17f55e3c.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bogotarot/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bogotarot/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bogotarot/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bogotarot/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bogotarot/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bogotarot/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bogotarot/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bogotarot/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bogotarot/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bogotarot/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bogotarot/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bogotarot/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bogotarot/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bogotarot/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bogotarot/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bogotarot/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
